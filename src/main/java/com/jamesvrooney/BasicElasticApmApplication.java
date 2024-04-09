987a7a2e01c7752c6e9a4d457072facf9db6d02d
jamesvrooney <jamesvrooney@hotmail.com>
2024-04-09 22:32:08 +0100

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/BasicElasticApmApplication.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicElasticApmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicElasticApmApplication.class, args);
+	}
+
+}
